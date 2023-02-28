6ab837d0bcdef78e12ca8589cdf4be34bcd9b368
jzagal <zagalvelozo@gmail.com>
2023-02-28 15:39:36 -0300

    feat: add author of the promgram

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+/**
+ * Hello World - A Simple C Program
+ *
+ * @package  Hello World
+ * @author   Jesus Zagal <zagalvelozo@gmail.com>
+ */
 #include <stdio.h>
 
 int main(int argc, char const *argv[])
